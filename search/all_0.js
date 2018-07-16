fc63b3cef06bc314e6b2b520713ffb56abe1c63f
Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
2018-07-16 16:55:19 +0200

    updated website

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['adc',['adc',['../classSeqBlock.html#a586761ad0a550d19c848fde2426c0461',1,'SeqBlock']]],
-  ['adcevent',['ADCEvent',['../structADCEvent.html',1,'']]],
-  ['amplitude',['amplitude',['../structRFEvent.html#ae2298eb23621add681421447389d80e6',1,'RFEvent::amplitude()'],['../structGradEvent.html#a87312b1c7ec58c74a2cede5ada8da54f',1,'GradEvent::amplitude()']]]
+  ['introduction',['Introduction',['../index.html',1,'']]]
 ];
--- search/all_1.js
+++ search/all_1.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['buildfileindex',['buildFileIndex',['../classExternalSequence.html#a7cfd1aeba9bac7a5f110942780475cc7',1,'ExternalSequence']]]
+  ['matlab_20toolbox',['MATLAB toolbox',['../matlab.html',1,'']]],
+  ['matlab_20example',['MATLAB example',['../matlabRead.html',1,'']]],
+  ['matlab_20example',['MATLAB example',['../matlabWriteGre.html',1,'']]],
+  ['matlab_20example',['MATLAB example',['../matlabWriteRf.html',1,'']]],
+  ['matlab_20sequence_20class',['MATLAB sequence class',['../SequenceCode.html',1,'']]]
 ];
